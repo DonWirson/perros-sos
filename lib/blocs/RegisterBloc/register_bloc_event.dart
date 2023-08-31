1f793dbebc08c0afe19896199818eb8943526333
DonWirson <twilliamson.valdes@gmail.com>
2023-08-31 12:55:36 -0400

    Agrega prueba de blocs para autenticación de usuario

---
+++ lib/blocs/RegisterBloc/register_bloc_event.dart
@@ -0,0 +1,4 @@
+part of 'register_bloc_bloc.dart';
+
+@immutable
+sealed class RegisterBlocEvent {}
---
+++ lib/blocs/RegisterBloc/register_bloc_state.dart
@@ -0,0 +1,6 @@
+part of 'register_bloc_bloc.dart';
+
+@immutable
+sealed class RegisterBlocState {}
+
+final class RegisterBlocInitial extends RegisterBlocState {}
