7e0be549c14089e6ff584bddbe610b0c966aef80
DonWirson <twilliamson.valdes@gmail.com>
2023-11-06 18:41:55 -0300

    mueve rutas y blocobserver a config

---
+++ lib/config/routes/routes.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+enum Routes {
+  landing(name: "landing", path: "/", screenWidget: LandingPage()),
+  login(name: "login", path: "/login", screenWidget: LoginPage()),
+  register(name: "register", path: "/register", screenWidget: RegisterPage()),
+  settings(name: "settings", path: "/settings", screenWidget: SettingsPage());
+
+  static GoRouter get getroutes {
+    return GoRouter(
+      initialLocation: "/login",
+      routes: Routes.values
+          .map((route) => GoRoute(
+                path: route.path,
+                name: route.name,
+                builder: (context, state) => route.screenWidget,
+              ))
+          .toList(),
+    );
+  }
+
+  const Routes(
+      {required this.name, required this.path, required this.screenWidget});
+  final String name;
+  final String path;
+  final Widget screenWidget;
+}
