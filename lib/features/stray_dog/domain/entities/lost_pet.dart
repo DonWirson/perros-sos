0abf2d5f108855f0fa5f4ef55b1ee34fd9365a1e
DonWirson <twilliamson.valdes@gmail.com>
2025-01-22 17:37:13 -0300

    chore(lost-pets): updated lostpet entity/model
    added new fields

--- lib/features/stray_dog/domain/entities/lost_pet.dart
+++ lib/features/stray_dog/domain/entities/lost_pet.dart
@@ -1,10 +1,14 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'lost_pet_entities.dart';
 
 class LostPet {
   int id;
   String name;
   DateTime reportDate;
+  String? description;
+  String? imageUrl;
+  String? distinctFeature;
+  int? phoneNumberInscribed;
+
   bool isActive;
   int animalTypeId;
   int genderId;
@@ -15,6 +19,10 @@ class LostPet {
     required this.id,
     required this.name,
     required this.reportDate,
+    required this.description,
+    required this.imageUrl,
+    required this.distinctFeature,
+    required this.phoneNumberInscribed,
     required this.isActive,
     required this.animalTypeId,
     required this.genderId,
